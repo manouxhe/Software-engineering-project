f5d79aa1ee4855a19bd4dda1d24987decbb778b4
DocteurGaufre <michaelspackitch@gmail.com>
2026-03-17 08:49:56 +0100

    Add info to Supplier

--- KitBox/Models/Supplier.cs
+++ KitBox/Models/Supplier.cs
@@ -2,10 +2,16 @@ namespace KitBox.Models
 {
     public class Supplier
     {
-        // PK: id_fournisseur
+        // PK: id_fournisseur (ID_SUPPLIER)
         public int Id { get; set; }
 
-        // nom
         public string Name { get; set; } = string.Empty;
+        public string SubName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+
+        // Petite propriété pratique si on veut afficher l'adresse complète d'un coup dans l'interface Avalonia
+        public string FullAddress => $"{Address}, {PostalCode} {City}";
     }
 }
